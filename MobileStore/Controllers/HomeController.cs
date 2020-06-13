9013640c632d3774c0345bb4daf40553fd8005c3
shempa <shempa@mail.ru>
2020-06-13 16:33:20 +0300

    Начальный возврат модуля MobileStore

---
+++ MobileStore/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MobileStore.Models;
+
+namespace MobileStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        MobileContext db;
+        public HomeController(MobileContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Phones.ToList());
+        }
+    }
+}
---
+++ MobileStore/Models/MobileContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MobileStore.Models
+{
+    public class MobileContext:DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public MobileContext(DbContextOptions<MobileContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
